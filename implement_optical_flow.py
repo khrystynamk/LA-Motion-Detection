712ed480bd2ac2ac1eadfc1e05e14e9c39df9449
khrystynamk <khrystyna.mysak@gmail.com>
2024-04-20 16:32:31 +0300

    Cleaned-up some code

--- implement_optical_flow.py
+++ implement_optical_flow.py
@@ -6,21 +6,13 @@ import skimage.io
 import skimage.transform
 from functools import partial
 import matplotlib.pyplot as plt
-from builtin_optical_flow import read_first_frame, release_resources
 
 
 def poly_exp(f, c, sigma):
     """
-    Calculates the local polynomial expansion of a 2D signal, as described by Farneback
-
-    Uses separable normalized correlation
+    Calculate the local polynomial expansion of a 2D signal.
 
     $f ~ x^T A x + B^T x + C$
-
-    If f[i, j] and c[i, j] are the signal value and certainty of pixel (i, j) then
-    A[i, j] is a 2x2 array representing the quadratic term of the polynomial, B[i, j]
-    is a 2-element array representing the linear term, and C[i, j] is a scalar
-    representing the constant term.
 
     Parameters
     ----------
@@ -34,11 +26,11 @@ def poly_exp(f, c, sigma):
     Returns
     -------
     A
-        Quadratic term of polynomial expansion
+        Quadratic term of polynomial expansion (Symmetric matrix)
     B
-        Linear term of polynomial expansion
+        Linear term of polynomial expansion (Vector)
     C
-        Constant term of polynomial expansion
+        Constant term of polynomial expansion (Scalar)
     """
 
     # ----- [Equivalent Correlation Kernels section in the paper] -----
@@ -46,16 +38,13 @@ def poly_exp(f, c, sigma):
     # Calculate applicability kernel (1D because it is separable, computation is significantly more efficient)
     n = int(4 * sigma + 1)
     x = np.arange(-n, n + 1, dtype=int)
-    a = np.exp(-(x**2) / (2 * sigma**2))  # a: applicability kernel [n]
+    a = np.exp(-(x**2) / (2 * sigma**2))  # applicability kernel
 
-    # b: calculate b from the paper. Calculate separately for X and Y dimensions
-    # [n, 6]
     # bx array has a shape determined by the shape of the applicability kernel a 
     # with an additional dimension for the different terms in the polynomial basis
 
     # ----- [Estimating the Coefficients of a Polynomial Model] -----
     # polynomial basis, {1, x, y, x^2, y^2, xy}
-    # y = 1
     bx = np.stack(
         [np.ones(a.shape), x, np.ones(a.shape), x**2, np.ones(a.shape), x], axis=-1
     )
@@ -71,8 +60,7 @@ def poly_exp(f, c, sigma):
         axis=-1,
     )
 
-    # Pre-calculate product of certainty and signal
-    cf = c * f
+    cf = c * f # product of the signal and its certainty
 
     # ----- [Cartesian Separability section in the paper] -----
     # The goal is to find the coefficients of a second-order polynomial that best fits the local signal.
@@ -82,32 +70,25 @@ def poly_exp(f, c, sigma):
     G = np.empty(list(f.shape) + [bx.shape[-1]] * 2)
     v = np.empty(list(f.shape) + [bx.shape[-1]])
 
-    # Apply separable cross-correlations
-
-    # Pre-calculate quantities recommended in paper
+    # ----- [Separable Cross-correlations for X] -----
     # einsum does multiplication, summation and transposition faster
-    # i,ij->ij A has one axis i, B has 2 axes (i and j) -- dimension labels
+    # i,ij->ij A has one axis i, B has 2 axes (i and j) - dimension labels
     ab = np.einsum("i,ij->ij", a, bx) # inner product (a · bm), this can be rewritten as (a[:, np.newaxis] * bx)
     abb = np.einsum("ij,ik->ijk", ab, bx) # inner product (a · bm, bm), this can be rewritten as abb = np.matmul(ab[:, :, None], bx[:, None, :]) or ab[:, :, None] @ bx[:, None, :], where @ is shortcut for matmul
 
-    # Calculate G and v for each pixel x with cross-correlation
     for i in range(bx.shape[-1]):
         for j in range(bx.shape[-1]):
             G[..., i, j] = scipy.ndimage.correlate1d(
-                c, abb[..., i, j], axis=0, mode="constant", cval=0 #‘constant’ (k k k k | a b c d | k k k k)
-                # The input is extended by filling all values beyond the edge with cval
-            )
+                c, abb[..., i, j], axis=0, mode="constant", cval=0 # The input is extended by filling all values beyond the edge with cval
+            ) # [..., i, j] is a shortcut to indicate that all axes preceding or following it should be fully included
 
         v[..., i] = scipy.ndimage.correlate1d(
             cf, ab[..., i], axis=0, mode="constant", cval=0
         )
 
-    # Pre-calculate quantities recommended in paper
-    ab = np.einsum("i,ij->ij", a, by) # inner product (a · bk), can be rewritten as (a[:, np.newaxis] * by)
-    abb = np.einsum("ij,ik->ijk", ab, by) # inner product (a · bk, bk), can be rewritten as abb = np.matmul(ab[:, :, None], by[:, None, :]) or ab[:, :, None] @ by[:, None, :], where @ is shortcut for matmul
-
-    # Calculate G and v for each pixel y with cross-correlation
-    # [..., i, j] is a shortcut to indicate that all axes preceding or following it should be fully included
+    # ----- [Separable Cross-correlations for Y] -----
+    ab = np.einsum("i,ij->ij", a, by)
+    abb = np.einsum("ij,ik->ijk", ab, by)
 
     for i in range(bx.shape[-1]):
         for j in range(bx.shape[-1]):
@@ -118,25 +99,20 @@ def poly_exp(f, c, sigma):
         v[..., i] = scipy.ndimage.correlate1d(
             v[..., i], ab[..., i], axis=1, mode="constant", cval=0
         )
-
-    # Solve r for each pixel
     r = np.linalg.solve(G, v)
 
     # ----- [Estimating the Coefficients of a Polynomial Model section in the paper] -----
 
-    # Quadratic term
     A = np.empty(list(f.shape) + [2, 2])
     A[..., 0, 0] = r[..., 3] # r_4
     A[..., 0, 1] = r[..., 5] / 2 # r_6 / 2
     A[..., 1, 0] = A[..., 0, 1] # r_6 / 2
     A[..., 1, 1] = r[..., 4] # r_5
 
-    # Linear term
     B = np.empty(list(f.shape) + [2])
     B[..., 0] = r[..., 1] # r_2
     B[..., 1] = r[..., 2] # r_3
 
-    # constant term
     C = r[..., 0] # r_1
 
     return A, B, C
@@ -172,10 +148,8 @@ def flow_iterative(
     d
         Optical flow field. d[i, j] is the (y, x) displacement for pixel (i, j)
     """
-
-    # Calculate the polynomial expansion at each point in the images
-    A1, B1, C1 = poly_exp(f1, c1, sigma)
-    A2, B2, C2 = poly_exp(f2, c2, sigma)
+    A1, B1, _ = poly_exp(f1, c1, sigma)
+    A2, B2, _ = poly_exp(f2, c2, sigma)
 
     # Pixel coordinates of each point in the images
     x = np.stack(
@@ -183,11 +157,10 @@ def flow_iterative(
         axis=-1,
     ).astype(int)
 
-    # Initialize displacement field
     if d is None:
         d = np.zeros(list(f1.shape) + [2])
 
-    # Set up applicability convolution window
+    # Applicability convolution window
     n_flow = int(4 * sigma_flow + 1)
     xw = np.arange(-n_flow, n_flow + 1)
     w = np.exp(-(xw**2) / (2 * sigma_flow**2))
@@ -196,18 +169,12 @@ def flow_iterative(
     S = np.eye(2)
     S_T = S.swapaxes(-1, -2)
 
-    # Iterate convolutions to estimate the optical flow
     for _ in range(num_iter):
-        # Set d~ as displacement field fit to nearest pixel (and constrain to not
-        # being off image). Note we are setting certainty to 0 for points that
-        # would have been off-image had we not constrained them
+        # Set d~ as displacement field fit to nearest pixel (and constrain to not being off image)
         d_ = d.astype(int)
         x_ = x + d_
 
-        # x_ = np.maximum(np.minimum(x_, np.array(f1.shape) - 1), 0)
-
-        # Constrain d~ to be on-image, and find points that would have
-        # been off-image
+        # Constrain d~ to be on-image, and find points that would have been off-image
         x_2 = np.maximum(np.minimum(x_, np.array(f1.shape) - 1), 0)
         off_f = np.any(x_ != x_2, axis=-1)
         x_ = x_2
@@ -216,7 +183,7 @@ def flow_iterative(
         c_ = c1[x_[..., 0], x_[..., 1]]
         c_[off_f] = 0
 
-        # Calculate A and delB for each point, according to paper, add in certainty by applying to A and delB
+        # Calculate A, deltaB for each point, according to paper
         A = (A1 + A2[x_[..., 0], x_[..., 1]]) / 2
         A *= c_[
             ..., None, None
@@ -227,11 +194,9 @@ def flow_iterative(
             ..., None
         ]
 
-        # Pre-calculate quantities recommended by paper
         A_T = A.swapaxes(-1, -2)
         ATA = S_T @ A_T @ A @ S
         ATb = (S_T @ A_T @ delB[..., None])[..., 0]
-        # btb = delB.swapaxes(-1, -2) @ delB
 
         # Apply separable cross-correlation to calculate linear equation
         # for each pixel: G*d = h
@@ -247,14 +212,8 @@ def flow_iterative(
 
 def main(video_path):
     """
-    Compares this implementation of Farneback's algorithms to OpenCV's implementation
-    of a similar version of the algorithm
+    Compares the built-in CV algorithm with the implemented one.
     """
-
-    # ---------------------------------------------------------------
-    # get images to calculate flow for
-    # ---------------------------------------------------------------
-
     cap = cv2.VideoCapture(video_path)
 
     ret, frame1 = cap.read()
@@ -270,15 +229,8 @@ def main(video_path):
     gray_prev = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
     gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
-    # Convert frames to double precision for optical flow calculation
     f1 = gray_prev.astype(np.double)
     f2 = gray.astype(np.double)
-
-    # certainties for images - certainty is decreased for pixels near the edge
-    # of the image, as recommended by Farneback
-
-    # c1 = np.ones_like(f1)
-    # c2 = np.ones_like(f2)
 
     c1 = np.minimum(
         1, 1 / 5 * np.minimum(np.arange(f1.shape[0])[:, None], np.arange(f1.shape[1]))
@@ -293,95 +245,65 @@ def main(video_path):
         ),
     )
     c2 = c1
-
-    # ---------------------------------------------------------------
-    # calculate optical flow with this algorithm
-    # ---------------------------------------------------------------
-
-    n_pyr = 4
-
-    # version using no regularization model
-    opts = dict(
+    pyramid_layers = 4
+    options = dict(
         sigma=4.0,
         sigma_flow=4.0,
         num_iter=3
     )
+    flow_field = None
 
-    # optical flow field
-    d = None
-
-    # calculate optical flow using pyramids
-    # note: reversed(...) because we start with the smallest pyramid
+    # start with the top (smallest) layer in the pyramid
     for pyr1, pyr2, c1_, c2_ in reversed(
         list(
             zip(
                 *list(
                     map(
-                        partial(skimage.transform.pyramid_gaussian, max_layer=n_pyr),
+                        partial(skimage.transform.pyramid_gaussian, max_layer=pyramid_layers),
                         [f1, f2, c1, c2],
                     )
                 )
             )
         )
     ):
-        if d is not None:
-            d = skimage.transform.pyramid_expand(d, channel_axis=-1)
-            d = d[: pyr1.shape[0], : pyr2.shape[1]] * 2
+        if flow_field is not None:
+            flow_field = skimage.transform.pyramid_expand(flow_field, channel_axis=-1)
+            flow_field = flow_field[: pyr1.shape[0], : pyr2.shape[1]] * 2
+        flow_field = flow_iterative(pyr1, pyr2, c1=c1_, c2=c2_, d=flow_field, **options)
 
-        d = flow_iterative(pyr1, pyr2, c1=c1_, c2=c2_, d=d, **opts)
-
-    xw = d + np.moveaxis(np.indices(f1.shape), 0, -1)
-
-    # ---------------------------------------------------------------
-    # calculate optical flow with opencv
-    # ---------------------------------------------------------------
-
-    opts_cv = dict(
+    options_cv = dict(
         pyr_scale=0.5,
-        levels=6,
-        winsize=25,
-        iterations=10,
-        poly_n=25,
-        poly_sigma=3.0,
-        # flags=0
+        levels=4,
+        winsize=15,
+        iterations=3,
+        poly_n=5,
+        poly_sigma=1.2,
         flags=cv2.OPTFLOW_FARNEBACK_GAUSSIAN,
     )
 
-    d2 = cv2.calcOpticalFlowFarneback(
-        f2.astype(np.uint8), f1.astype(np.uint8), None, **opts_cv
+    flow_field_cv = cv2.calcOpticalFlowFarneback(
+        f2.astype(np.uint8), f1.astype(np.uint8), None, **options_cv
     )
-    d2 = -d2[..., (1, 0)]
+    flow_field_cv = -flow_field_cv[..., (1, 0)]
 
-    xw2 = d2 + np.moveaxis(np.indices(f1.shape), 0, -1)
-
-    # ---------------------------------------------------------------
-    # use calculated optical flow to warp images
-    # ---------------------------------------------------------------
-
-    # opencv warped frame
+    # warped frames
+    xw = flow_field + np.moveaxis(np.indices(f1.shape), 0, -1)
+    xw2 = flow_field_cv + np.moveaxis(np.indices(f1.shape), 0, -1)
     f2_w2 = skimage.transform.warp(f2, np.moveaxis(xw2, -1, 0), cval=np.nan)
-
-    # warped frame
     f2_w = skimage.transform.warp(f2, np.moveaxis(xw, -1, 0), cval=np.nan)
 
-    # ---------------------------------------------------------------
-    # visualize results
-    # ---------------------------------------------------------------
-
-    fig, axes = plt.subplots(2, 2, sharex=True, sharey=True)
-
-    p = 2.0  # percentile of histogram edges to chop off
+    _, axes = plt.subplots(2, 2, sharex=True, sharey=True)
+    p = 2.0
     vmin, vmax = np.nanpercentile(f1 - f2, [p, 100 - p])
-    cmap = "gray"
 
-    axes[0, 0].imshow(f1, cmap=cmap)
+    axes[0, 0].imshow(f1, cmap="gray")
     axes[0, 0].set_title("f1 (fixed image)")
-    axes[0, 1].imshow(f2, cmap=cmap)
+    axes[0, 1].imshow(f2, cmap="gray")
     axes[0, 1].set_title("f2 (moving image)")
-    axes[1, 0].imshow(f1 - f2_w2, cmap=cmap, vmin=vmin, vmax=vmax)
-    axes[1, 0].set_title("difference f1 - f2 warped: opencv implementation")
-    axes[1, 1].imshow(f1 - f2_w, cmap=cmap, vmin=vmin, vmax=vmax)
-    axes[1, 1].set_title("difference f1 - f2 warped: this implementation")
+    axes[1, 0].imshow(f1 - f2_w2, cmap="gray", vmin=vmin, vmax=vmax)
+    axes[1, 0].set_title("f1 - f2 warped (opencv)")
+    axes[1, 1].imshow(f1 - f2_w, cmap="gray", vmin=vmin, vmax=vmax)
+    axes[1, 1].set_title("f1 - f2 warped (implementation)")
 
     plt.show()
 
